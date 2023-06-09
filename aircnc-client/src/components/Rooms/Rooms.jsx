6f1e036b2dbbfcc2cb4897a7ebf0de68c044cc31
asif247 <86229363+asif247@users.noreply.github.com>
2023-06-10 03:21:30 +0600

    footer and custom heading added

--- aircnc-client/src/components/Rooms/Rooms.jsx
+++ aircnc-client/src/components/Rooms/Rooms.jsx
@@ -3,6 +3,7 @@ import Container from '../Shared/Container';
 import Card from './Card';
 import Loader from '../Shared/Loader/Loader';
 import { useSearchParams } from 'react-router-dom';
+import Heading from '../Heading/Heading';
 
 const Rooms = () => {
     const [rooms, setRooms] = useState([])
@@ -14,24 +15,38 @@ const Rooms = () => {
         fetch('./rooms.json')
             .then(res => res.json())
             .then(data => {
-                setRooms(data)
+                if (category) {
+                    const filtered = data.filter(room => room.category === category)
+                    setRooms(filtered)
+                }
+                else {
+                    setRooms(data)
+                }
                 setLoading(false)
         })
-    }, [])
+    }, [category])
     if (loading) {
         return <Loader/>
       }
     return (
         <div>
             <Container>
-                <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-8'>
+                {
+                    rooms && rooms.length>0? <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-8'>
                     {
                         rooms.map((room, i) => <Card
                             key={i}
                             room={room}
                         ></Card>)
                     }
+                    </div> : <div className='pt-12'>
+                            <Heading
+                                title="No rooms available in this category"
+                                subtitle="Please select other categories"
+                                center={true}
+                            ></Heading>
                 </div>
+                }
             </Container>
         </div>
     );
--- aircnc-client/src/layouts/Main.jsx
+++ aircnc-client/src/layouts/Main.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "../components/Shared/NavBar/NavBar"
+import Footer from "../components/Shared/Footer/Footer"
 
 const Main = () => {
   return (
     <div>
       <NavBar />
       <div className="pt-28 pb-20">
-      <Outlet/>
+        <Outlet />
+        <Footer/>
       </div>
     </div>
   )
