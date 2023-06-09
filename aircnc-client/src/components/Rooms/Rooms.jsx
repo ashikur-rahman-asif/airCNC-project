5b3425fd508ca2134a32b2a7c570de69268692f6
asif247 <86229363+asif247@users.noreply.github.com>
2023-06-10 02:48:15 +0600

    useParams added(grab the last url txt)

--- aircnc-client/src/components/Rooms/Rooms.jsx
+++ aircnc-client/src/components/Rooms/Rooms.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 import Container from '../Shared/Container';
 import Card from './Card';
 import Loader from '../Shared/Loader/Loader';
+import { useSearchParams } from 'react-router-dom';
 
 const Rooms = () => {
     const [rooms, setRooms] = useState([])
-    const [loading,setLoading]=useState(false)
+    const [loading, setLoading] = useState(false)
+    const [params, setParams] = useSearchParams()
+    const category = params.get('category')
     useEffect(() => {
         setLoading(true)
         fetch('./rooms.json')
