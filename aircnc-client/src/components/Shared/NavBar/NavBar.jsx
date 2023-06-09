b1b5ebda3f231cb15dadd1ccc9b4f187fc7b0b74
asif247 <86229363+asif247@users.noreply.github.com>
2023-06-09 16:30:56 +0600

    common style(wrapper) added

---
+++ aircnc-client/src/components/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='fixed w-full bg-white shadow-sm z-10'>
+            <div className="py-4 border-b-[1px]">
+                Asif
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
