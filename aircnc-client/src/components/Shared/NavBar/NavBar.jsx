478fc0c6e95fb0361c2899a6af7cfc072d618d10
asif247 <86229363+asif247@users.noreply.github.com>
2023-06-09 17:10:27 +0600

    navbar done

--- aircnc-client/src/components/Shared/NavBar/NavBar.jsx
+++ aircnc-client/src/components/Shared/NavBar/NavBar.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import Container from '../Container';
+import logo from "../../../assets/images/logo.png";
+import { Link } from 'react-router-dom';
+import Search from './Search';
+import MenuDropdown from './MenuDropDown';
 const NavBar = () => {
     return (
         <div className='fixed w-full bg-white shadow-sm z-10'>
             <div className="py-4 border-b-[1px]">
-                Asif
+                <Container>
+                    <div className='flex flew-row items-center justify-between gap-3 md:gap-0'>
+                        <div>
+                            <Link><img src={logo} alt="air-cnc-logo" className='w-[100px] h-[40px] hidden md:block' /></Link>
+                        </div>
+                        <Search/>
+                        <MenuDropdown/>
+                    </div>
+                </Container>
             </div>
         </div>
     );
