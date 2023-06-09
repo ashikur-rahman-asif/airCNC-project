aaf046e5fd47c46ded0f158ea3e1ebf8d4f73de0
asif247 <86229363+asif247@users.noreply.github.com>
2023-06-09 16:10:24 +0600

    initial setup done

---
+++ aircnc-client/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfLtB6F2vPNI5Hjz6-yrvQ43yN8PuOq44",
+  authDomain: "air-cnc-c7de5.firebaseapp.com",
+  projectId: "air-cnc-c7de5",
+  storageBucket: "air-cnc-c7de5.appspot.com",
+  messagingSenderId: "927773842492",
+  appId: "1:927773842492:web:9b63d2b5ee64711ee33739"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
